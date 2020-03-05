62d988cfad7a89e33bd149be9dace949c68110eb
joellAugustine <joell.augustine@gmail.com>
2020-03-05 13:55:22 -0700

    this is lesson 3

---
+++ app/src/main/java/com/hfad/smack/ui/LoginActivity.kt
@@ -0,0 +1,22 @@
+package com.hfad.smack.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.hfad.smack.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun loginCreateUserBtnClicked(view: View){
+
+    }
+    fun loginLoginBtnClicked (view: View) {
+
+    }
+    }
+
