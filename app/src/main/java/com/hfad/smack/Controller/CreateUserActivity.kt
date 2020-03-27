b78ca30e98b4e4a0a701ea54b82fdd856c85af80
joellAugustine <joell.augustine@gmail.com>
2020-03-27 10:30:45 -0600

    lesson 12

--- app/src/main/java/com/hfad/smack/Controller/CreateUserActivity.kt
+++ app/src/main/java/com/hfad/smack/Controller/CreateUserActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import com.hfad.smack.R
 import com.hfad.smack.Services.AuthService
+import com.hfad.smack.Services.UserDataService
 import kotlinx.android.synthetic.main.activity_create_user.*
 import java.util.*
 
@@ -49,14 +50,27 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun createUserClicked(view: View) {
+        val userName = createUserNameText.text.toString()
         val email = createEmailText.text.toString()
         val password = createPasswordText.text.toString()
         AuthService.registerUser(this, email, password) { registerSuccess ->
             if (registerSuccess) {
                 AuthService.loginUser(this, email, password) { loginSuccess ->
                     if (loginSuccess) {
-                    println(AuthService.authToken)
-                        println(AuthService.userEmail)
+                        AuthService.createUser(
+                            this,
+                            userName,
+                            email,
+                            userAvatar,
+                            avatarColor
+                        ) { createSuccess ->
+                            if (createSuccess) {
+                                println(UserDataService.avatarName)
+                                println(UserDataService.avatarColor)
+                                println(UserDataService.name)
+                                finish()
+                            }
+                        }
                     }
                 }
             }
--- app/src/main/java/com/hfad/smack/Services/AuthService.kt
+++ app/src/main/java/com/hfad/smack/Services/AuthService.kt
@@ -6,6 +6,7 @@ import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.hfad.smack.Utilities.URL_Create_User
 import com.hfad.smack.Utilities.URL_LOGIN
 import com.hfad.smack.Utilities.URL_REGISTER
 import org.json.JSONException
@@ -14,15 +15,16 @@ import org.json.JSONObject
 
 object AuthService {
 
-    var  isLoggedIn = false
-    var  userEmail = ""
+    var isLoggedIn = false
+    var userEmail = ""
     var authToken = ""
 
     fun registerUser(
         context: Context,
         email: String,
         password: String,
-        complete: (Boolean) -> Unit) {
+        complete: (Boolean) -> Unit
+    ) {
 
         val jsonBody = JSONObject()
         jsonBody.put("email", email)
@@ -59,16 +61,16 @@ object AuthService {
         jsonBody.put("password", password)
         val requestBody = jsonBody.toString()
         val loginRequest =
-            object : JsonObjectRequest(Method.POST, URL_LOGIN, null, Response.Listener { response->
+            object : JsonObjectRequest(Method.POST, URL_LOGIN, null, Response.Listener { response ->
                 try {
                     userEmail = response.getString("user")
                     authToken = response.getString("token")
                     isLoggedIn = true
-                    complete (true)
-                } catch (e: JSONException){
+                    complete(true)
+                } catch (e: JSONException) {
                     Log.d("JSON", "EXC:" + e.localizedMessage)
                 }
-            }, Response.ErrorListener { error->
+            }, Response.ErrorListener { error ->
                 Log.d("ERROR", "Could not register user: $error")
                 complete(false)
 
@@ -85,5 +87,64 @@ object AuthService {
         Volley.newRequestQueue(context).add(loginRequest)
 
     }
+
+    fun createUser(
+        context: Context,
+        name: String,
+        email: String,
+        avatarName: String,
+        avatarColor: String,
+        complete: (Boolean) -> Unit
+    ) {
+
+        val jsonBody = JSONObject()
+        jsonBody.put("name", name)
+        jsonBody.put("email", email)
+        jsonBody.put("avatarName", avatarName)
+        jsonBody.put("avatarColor", avatarColor)
+        val requestBody = jsonBody.toString()
+
+        val createRequest = object :
+            JsonObjectRequest(Method.POST, URL_Create_User, null, Response.Listener { response ->
+
+                try {
+
+                    UserDataService.name = response.getString("name")
+                    UserDataService.email = response.getString("email")
+                    UserDataService.avatarName = response.getString("avatarName")
+                    UserDataService.avatarColor = response.getString("avatarColor")
+                    UserDataService.id = response.getString("_id")
+                    complete(true)
+
+                } catch (e: JSONException) {
+                    Log.d("JSON", "EXC" + e.localizedMessage)
+                    complete(false)
+                }
+            }, Response.ErrorListener { error ->
+                Log.d("ERROR", "Could not add user $error")
+                complete(false)
+
+            }) {
+
+            override fun getBodyContentType(): String {
+                return "application/json; charset=utf-8"
+            }
+
+            override fun getBody(): ByteArray {
+
+                return requestBody.toByteArray()
+            }
+
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                val put = headers.put("Authorization", "Bearer $authToken")
+                return headers
+
+        }
+    }
+
+        Volley.newRequestQueue(context).add(createRequest)
 }
+}
+
 
