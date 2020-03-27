7921a81ee8c4e0ebbb7fe4c3cd3181a0e23979b9
joellAugustine <joell.augustine@gmail.com>
2020-03-26 18:29:55 -0600

    next lesson

--- app/src/main/java/com/hfad/smack/Controller/CreateUserActivity.kt
+++ app/src/main/java/com/hfad/smack/Controller/CreateUserActivity.kt
@@ -19,39 +19,49 @@ class CreateUserActivity : AppCompatActivity() {
         setContentView(R.layout.activity_create_user)
     }
 
-    fun generateUserAvatar (view: View){
+    fun generateUserAvatar(view: View) {
         val random = Random()
         val color = random.nextInt(2)
         val avatar = random.nextInt(28)
 
-        if (color==0) {
+        if (color == 0) {
             userAvatar = "light$avatar"
-        }else{
+        } else {
             userAvatar = "dark$avatar"
         }
         val resourceID = resources.getIdentifier(userAvatar, "drawable", packageName)
         createAvatarImageView.setImageResource(resourceID)
-     }
+    }
 
-    fun generateColorClicked (view: View){
+    fun generateColorClicked(view: View) {
         val random = Random()
         val r = random.nextInt(255)
         val g = random.nextInt(255)
         val b = random.nextInt(255)
-        createAvatarImageView.setBackgroundColor(Color.rgb(r,g,b))
+        createAvatarImageView.setBackgroundColor(Color.rgb(r, g, b))
 
-        val savedR = r.toDouble()/255
-        val savedG = g.toDouble()/255
-        val savedB = b.toDouble()/255
+        val savedR = r.toDouble() / 255
+        val savedG = g.toDouble() / 255
+        val savedB = b.toDouble() / 255
 
         avatarColor = "[$savedR, $savedG, $savedB, 1]"
         //println(avatarColor)
     }
 
-    fun createUserClicked (view: View) {
-        AuthService.registerUser(this, "jbob.com", "123456") { complete ->
-            if (complete) {
+    fun createUserClicked(view: View) {
+        val email = createEmailText.text.toString()
+        val password = createPasswordText.text.toString()
+        AuthService.registerUser(this, email, password) { registerSuccess ->
+            if (registerSuccess) {
+                AuthService.loginUser(this, email, password) { loginSuccess ->
+                    if (loginSuccess) {
+                    println(AuthService.authToken)
+                        println(AuthService.userEmail)
+                    }
+                }
             }
         }
     }
 }
+
+
